952ab314a628230035d668c6d49c493c89900da3
tanakaka773 <yamadataro881@gmail.com>
2020-08-10 20:34:00 +0900

    add-destroy.edit.update

--- app/controllers/remarks_controller.rb
+++ app/controllers/remarks_controller.rb
@@ -4,4 +4,31 @@ class RemarksController < ApplicationController
     @remarks = Remark.all
   end
 
+  def new
+    @remark = Remark.new
+  end
+
+  def create
+    Remark.create(remark_params)
+  end
+
+  def destroy
+    remark = Remark.find(params[:id])
+    remark.destroy
+  end
+
+  def edit
+    @remark = Remark.find(params[:id])
+  end
+
+  def update
+    remark = Remark.find(params[:id])
+    remark.update(remark_params)
+  end
+
+  private
+  def remark_params
+    params.require(:remark).permit(:name, :image, :text)
+  end
+
 end
